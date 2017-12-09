1f4ceaeccbcd66c86662ebb23cec1a4a19c7e193
heyf <tonyheyifan@gmail.com>
2017-12-10 00:31:38 +0800

    Day 4: 119 O(k)

--- leetcode/119_pascals-triangle-ii.py
+++ leetcode/119_pascals-triangle-ii.py
@@ -28,3 +28,42 @@ class Solution(object):
         
 s = Solution()
 print(s.getRow(3))
+
+
+'''Solution 2: O(k)'''
+#!/usr/bin/python3
+# 119. Pascal's Triangle II - LeetCode
+# https://leetcode.com/problems/pascals-triangle-ii/description/
+
+class Solution(object):
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        res = [1]
+        for i in range(1,rowIndex+1):
+            j = 1
+            cur = 1
+            while j < i:
+                prev = cur
+                cur = res[j]
+                res[j] = prev + cur
+                j += 1
+            res.append(1)
+        return res
+
+pairs = [
+    (0,[1]),
+    (1,[1,1]),
+    (2,[1,2,1]),
+    (3,[1, 3, 3, 1]),
+    (4,[1, 4, 6, 4, 1]),
+    (5,[1, 5, 10, 10, 5, 1]),
+    (6,[1, 6, 15, 20, 15, 6, 1]),
+    (7,[1, 7, 21, 35, 35, 21, 7, 1])
+]
+        
+s = Solution()
+for i in pairs:
+    print(s.getRow(i[0]), s.getRow(i[0])==i[1])
