ab4b68d2a9a11a6bc21ac569f49cad29f744c46c
heyf <tonyheyifan@gmail.com>
2019-06-02 12:08:59 +0800

    1073: WA3

--- leetcode/1073.py
+++ leetcode/1073.py
@@ -1,24 +1,4 @@
 class Solution:
-    def addBinary(self, arr1, arr2):
-        result = []
-        c = 0
-        while arr1 or arr2:
-            a = arr1.pop(-1) if arr1 else 0
-            b = arr2.pop(-1) if arr2 else 0
-            a = a + b + c
-            if a == 3:
-                result.insert(0,1) 
-                c = 1
-            elif a == 2:
-                result.insert(0,0) 
-                c = 1
-            else:
-                result.insert(0,a) 
-                c = 0
-        if c == 1:
-            result.insert(0,1)
-        return result
-
     def addNegabinary(self, arr1, arr2):
         result = []
         c = 0
@@ -34,6 +14,10 @@ class Solution:
                 c = 0
         if c == 1:
             result = [1,1] + result
+        while result and result[0] == 0:
+            result.pop(0)
+        if not result:
+            return [0]
         return result
 
 # 1,0 + 1,0 = 1,1,0,0
