ae17b3a408c1f7b074a4a46fc546d56a81f743bc
heyf <tonyheyifan@gmail.com>
2019-06-02 12:15:37 +0800

    1073: WA4

--- leetcode/1073.py
+++ leetcode/1073.py
@@ -5,14 +5,17 @@ class Solution:
         while arr1 or arr2:
             a = arr1.pop(-1) if arr1 else 0
             b = arr2.pop(-1) if arr2 else 0
-            a = a + b - c
+            a = a + b + c
             if a == 2:
                 result.insert(0,0) 
+                c = -1
+            elif a == -1:
+                result.insert(0,1)
                 c = 1
             else:
                 result.insert(0,a) 
                 c = 0
-        if c == 1:
+        if c == -1:
             result = [1,1] + result
         while result and result[0] == 0:
             result.pop(0)
@@ -24,4 +27,6 @@ class Solution:
 s = Solution()
 # print(s.addNegabinary(arr1 = [1,1,1,1,1], arr2 = [1,0,1]))
 # print(s.addNegabinary(arr1 = [1], arr2 = [1])) # [1,1,0]
-print(s.addNegabinary(arr1 = [1,1], arr2 = [1])) # [0]
+# print(s.addNegabinary(arr1 = [1,1], arr2 = [1])) # [0] # WA2
+print(s.addNegabinary(arr1 = [1], arr2 = [1,0,1])) # [1,1,0,1,0] # WA3
+print(s.addNegabinary(arr1 = [1,0,1], arr2 = [1,0,1])) # [1,1,1,1,0] # WA4
