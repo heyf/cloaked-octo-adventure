3155d8439c4ca101ea3194384755a5b71e412404
heyf <tonyheyifan@gmail.com>
2019-06-16 10:50:24 +0800

    5055

---
+++ leetcode/5055_robot-bounded-in-circle.py
@@ -0,0 +1,12 @@
+# Robot Bounded In Circle - LeetCode Contest
+# https://leetcode.com/contest/weekly-contest-136/problems/robot-bounded-in-circle/
+
+class Solution:
+    def isRobotBounded(self, instructions: str) -> bool:
+        pass
+
+ans = [
+    ["GGLLGG",True],
+    ["GG",False],
+    ["GL",True],
+]
