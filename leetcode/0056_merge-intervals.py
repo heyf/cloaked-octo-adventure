618908e7f8a6d4dbda761cc47a5d4ec0e2c43871
heyf <tonyheyifan@gmail.com>
2019-09-24 14:10:35 +0800

    plain iter

--- leetcode/0056_merge-intervals.py
+++ leetcode/0056_merge-intervals.py
@@ -7,6 +7,8 @@ from typing import List
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if not intervals:
+            return []
         intervals = sorted(intervals, key=lambda x: x[0])
         buffer_start, buffer_end = intervals[0]
         ret = []
@@ -16,10 +18,19 @@ class Solution:
                 ret.append([buffer_start,buffer_end])
                 buffer_start, buffer_end = start, end
             else:
-                buffer_end = end
+                buffer_end = max(buffer_end, end)
         ret.append([buffer_start, buffer_end])
         return ret
 
-# s = Solution()
-# print(s.merge([[1,3],[2,4]]))
+# class Test:
+#     @staticmethod
+#     def test():
+#         ans = [
+#             {"input":{"intervals":[[1,3],[2,4]]},"output":[[1,4]]},
+#             {"input":{"intervals":[]},"output":[[]]},
+#         ]
+#         s = Solution()
+#         for i in ans:
+#             assert str(s.merge(**i["input"])) == str(i["output"])
 
+# Test.test()
