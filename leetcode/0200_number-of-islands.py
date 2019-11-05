0aa5999ae8123725b6938f0ebfa404a17af85965
heyf <tonyheyifan@gmail.com>
2019-11-06 00:33:18 +0800

    0200: AC

--- leetcode/0200_number-of-islands.py
+++ leetcode/0200_number-of-islands.py
@@ -9,15 +9,15 @@ from typing import List
 # @lc code=start
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid and not grid[0]:
+        if not grid or not grid[0]:
             return 0
         m, n = len(grid), len(grid[0])
 
         def DFS(i,j):
             if i < 0 or j < 0 or i >= m or j >= n:
                 return 0
-            if grid[i][j] == 1:
-                grid[i][j] = 0
+            if grid[i][j] == "1":
+                grid[i][j] = "0"
                 DFS(i+1, j)
                 DFS(i-1, j)
                 DFS(i, j+1)
@@ -33,17 +33,17 @@ class Solution:
         return count
 
 # @lc code=end
-a = [
-    [1,1,1,1,0],
-    [1,1,0,1,0],
-    [1,1,0,0,0],
-    [0,0,0,0,0]
-]
 # a = [
-#     [1,1,0,0,0],
-#     [1,1,0,0,0],
-#     [0,0,1,0,0],
-#     [0,0,0,1,1]
+#     ["1","1","1","1","0"],
+#     ["1","1","0","1","0"],
+#     ["1","1","0","0","0"],
+#     ["0","0","0","0","0"]
 # ]
+a = [
+    ["1","1","0","0","0"],
+    ["1","1","0","0","0"],
+    ["0","0","1","0","0"],
+    ["0","0","0","1","1"]
+]
 s = Solution()
 print(s.numIslands(a))
