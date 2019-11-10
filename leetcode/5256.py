56c7cb500f80fb9af1a34874c64755763e9fbc5e
heyf <tonyheyifan@gmail.com>
2019-11-10 11:13:36 +0800

    WA2

--- leetcode/5256.py
+++ leetcode/5256.py
@@ -20,14 +20,24 @@ class Solution:
                     col_lower[idx] = 1
                     lower -= 1
                 else:
-                    return []
+                    return [[],[]]
+        
         return [ col_upper, col_lower ]
 
 s = Solution()
 # ret = s.reconstructMatrix(upper = 5, lower = 5, colsum = [2,1,2,0,1,0,1,2,1,1])
 # If no valid solution exists, return an empty 2-D array.
+
 # WA1
-ret = s.reconstructMatrix(2,
-3,
-[2,2,1,1]) # []
+# ret = s.reconstructMatrix(2,
+# 3,
+# [2,2,1,1]) # []
+
+# WA2
+ret = s.reconstructMatrix(
+    1,
+    4,
+    [2,1,2,0,0,2],
+)
+
 print(ret)
