dc91cef08c654877fc7a5d71359e78a9c492b0a6
heyf <tonyheyifan@gmail.com>
2019-11-16 15:18:20 +0800

    0213: RLE

---
+++ leetcode/0213_house-robber-ii.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=213 lang=python3
+#
+# [213] House Robber II
+#
+from typing import List
+
+# @lc code=start
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def helper(start, end):
+            if end < start:
+                return 0
+            return max(nums[end]+helper(0,end-2), helper(0,end-1))
+        end = len(nums) - 1
+        return max(nums[end]+helper(1,end-2), helper(0,end-1))
+
+# @lc code=end
+s = Solution()
+# ret = s.rob([2,3,2])
+ret = s.rob([1,2,3,1])
+print(ret)
