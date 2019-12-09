bb5411e2924fa657a1c5143ffbda943369adf0fe
heyf <tonyheyifan@gmail.com>
2019-12-09 13:36:26 +0800

    0138: AC

--- leetcode/0138_copy-list-with-random-pointer.py
+++ leetcode/0138_copy-list-with-random-pointer.py
@@ -9,18 +9,23 @@ class Node:
         self.val = val
         self.next = next
         self.random = random
+    
+    def __str__(self):
+        return f"Node({self.val})"
+# from collections import namedtuple
+# Node = namedtuple("Node", ["val", "next", "random"])
 
 # @lc code=start
 class Solution:
     def copyRandomList(self, head: 'Node') -> 'Node':
-        pointer_mapping = {}
+        pointer_mapping = {None:None}
         new_head = Node(-1, None, None)
         curr = head
         new_prev = new_head
         while curr:
             new_prev.next = Node(curr.val, None, None)
+            pointer_mapping[curr] = new_prev.next
             new_prev = new_prev.next
-            pointer_mapping[curr] = new_prev
             curr = curr.next
         curr = head
         new_curr = new_head.next
@@ -32,3 +37,11 @@ class Solution:
 
 # @lc code=end
 s = Solution()
+
+head = Node(val=-1,next=None,random=None)
+# head.random = head
+new_head = s.copyRandomList(head)
+
+while new_head:
+    print(new_head)
+    new_head = new_head.next
