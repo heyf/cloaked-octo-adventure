1cab865b2579538f3afe50b47945421f55eae955
heyf <tonyheyifan@gmail.com>
2019-12-10 13:50:42 +0800

    0029: WA1

---
+++ leetcode/0029_divide-two-integers.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode id=29 lang=python3
+#
+# [29] Divide Two Integers
+#
+
+# @lc code=start
+INT_MAX = 2 ** 31 - 1
+
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        m, n = abs(dividend), abs(divisor)
+        ret = 0
+        if n == 1:
+            ret = m
+        else:
+            while m >= n:
+                t, p = n, 1
+                while m >= (t << 1):
+                    t <<= 1
+                    p <<= 1
+                ret += p
+                m -= t
+        if ret > INT_MAX:
+            ret = INT_MAX
+        return ret if dividend > 0 ^ divisor > 0 else -ret
+
+# @lc code=end
+s = Solution()
+ret = s.divide(1, 1) # WA1
+print(ret)
