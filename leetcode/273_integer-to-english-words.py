4ae706716c4cb903787493a05ad93ceea7066f77
heyf <tonyheyifan@gmail.com>
2019-12-24 13:45:37 +0800

    0237: WA2

---
+++ leetcode/273_integer-to-english-words.py
@@ -0,0 +1,53 @@
+#
+# @lc app=leetcode id=273 lang=python3
+#
+# [273] Integer to English Words
+#
+
+# @lc code=start
+class Solution:
+    def numberToWords(self, num: int) -> str:
+        v0 = ["Thousand", "Million", "Billion"]
+        v1 = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
+        v2 = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
+        ret = ""
+
+        def convert_hundred(sub_num):
+            h_ret = []
+            hundred = sub_num // 100
+            if hundred > 0:
+                h_ret.append(v1[hundred])
+                h_ret.append("Hundred")
+            sub_num = sub_num % 100
+            if sub_num > 20:
+                ten = sub_num // 10
+                sub_num = sub_num % 10
+                h_ret.append(v2[ten])
+                if sub_num > 0:
+                    h_ret.append(v1[sub_num])
+            else:
+                h_ret.append(v1[sub_num])
+            return " ".join(h_ret)
+
+        sub_num = num % 1000
+        ret = convert_hundred(sub_num) + ret
+        num = num // 1000
+        for i in range(3):
+            if num == 0:
+                break
+            else:
+                ret = v0[i] + " " + ret
+            sub_num = num % 1000
+            ret = convert_hundred(sub_num) + " " + ret
+            num = num // 1000
+
+        return ret if ret else "Zero"
+
+# @lc code=end
+s = Solution()
+# WA1
+# num = 0
+# WA2
+num = 20
+ret = s.numberToWords(num)
+print(ret)
